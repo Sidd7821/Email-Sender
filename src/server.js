6fd94a1f38727e5e34cc20a3047fb92b8d39075b
Siddharth Kasundra <94774221+Sidd7821@users.noreply.github.com>
2025-07-15 14:29:34 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -35,7 +35,8 @@ app.use(cors({
     res.status(204).send();
   });
 
-app.use(express.json());
+app.use(express.json({limit : '50mb'}));
+app.use(express.urlencoded({ limit: '50mb', extended: true}));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 // Routes
